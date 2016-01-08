e8c1df12e849777d863d6d87bee36dd25aab5d6a
Sreejith K R <kr.srjith@gmail.com>
2016-01-08 22:20:45 +0530

    Change Log ___________ Initialise project

---
+++ SwiftyGCDAsyncSocket/SwiftyGCDAsyncSocket.swift
@@ -0,0 +1,45 @@
+//
+//  SwiftyGCDAsyncSocket.swift
+//  SwiftyGCDAsyncSocket
+//
+//  Created by Sreejith K R on 08/01/16.
+// 
+//
+
+import Foundation
+
+let  SwiftyGCDAsyncSocketException = "SwiftyGCDAsyncSocketException"
+let SwiftyGCDAsyncSocketErrorDomain = "SwiftyGCDAsyncSocketErrorDomain"
+
+let SwiftyGCDAsyncSocketQueueName = "SwiftyGCDAsyncSocketQueueName"
+let SwiftyGCDAsyncSocketThreadName = "SwiftyGCDAsyncSocketThreadName"
+
+let SwiftyGCDAsyncSocketManuallyEvaluateTrust = "SwiftyGCDAsyncSocketManuallyEvaluateTrust"
+let SwiftyGCDAsyncSocketUseCFStreamForTLS = "SwiftyGCDAsyncSocketUseCFStreamForTLS"
+
+let SwiftyGCDAsyncSocketSSLPeerID = "SwiftyGCDAsyncSocketSSLPeerID"
+let SwiftyGCDAsyncSocketSSLProtocolVersionMin = "SwiftyGCDAsyncSocketSSLProtocolVersionMin"
+let SwiftyGCDAsyncSocketSSLProtocolVersionMax = "SwiftyGCDAsyncSocketSSLProtocolVersionMax"
+let SwiftyGCDAsyncSocketSSLSessionOptionFalseStart = "SwiftyGCDAsyncSocketSSLSessionOptionFalseStart"
+let SwiftyGCDAsyncSocketSSLSessionOptionSendOneByteRecord = "SwiftyGCDAsyncSocketSSLSessionOptionSendOneByteRecord"
+let SwiftyGCDAsyncSocketSSLCipherSuites = "SwiftyGCDAsyncSocketSSLCipherSuites"
+
+let GCDAsyncSocketLoggingContext = 65535
+
+
+enum SwiftyGCDAsyncSocketError:Int{
+    case NoError = 0,           // Never used
+     BadConfigError,        // Invalid configuration
+     BadParamError,         // Invalid parameter was passed
+     ConnectTimeoutError,   // A connect operation timed out
+     ReadTimeoutError,      // A read operation timed out
+     WriteTimeoutError,     // A write operation timed out
+     ReadMaxedOutError,     // Reached set maxLength without completing
+     ClosedError,           // The remote peer closed the connection
+    OtherError;            // Description provided in userInfo
+}
+
+
+class SwiftyGCDAsyncSocket:NSObject {
+    
+}
---
+++ SwiftyGCDAsyncSocket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyGCDAsyncSocket
+//
+//  Created by Sreejith K R on 08/01/16.
+// 
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
