2eeefe8082b9a3243363199302f3b159b0558d3d
Sreejith K R <kr.srjith@gmail.com>
2016-01-09 01:19:33 +0530

    Change Log ___________
    Method definition for accepting defined.

--- SwiftyGCDAsyncSocket/SwiftyGCDAsyncSocket.swift
+++ SwiftyGCDAsyncSocket/SwiftyGCDAsyncSocket.swift
@@ -39,7 +39,59 @@ enum SwiftyGCDAsyncSocketError:Int{
     OtherError;            // Description provided in userInfo
 }
 
+class SwiftyGCDAsyncSocketDelegate:AnyObject{
 
+}
 class SwiftyGCDAsyncSocket:NSObject {
     
+    override init(){
+    
+    }
+    
+    init(socketQueue sq:dispatch_queue_t) {
+        
+    }
+    init(delegate adelegate:AnyObject, delegateQueue dq:dispatch_queue_t) {
+
+    }
+    
+    init(delegate adelegate:AnyObject, delegateQueue dq:dispatch_queue_t, socketQueue sq:dispatch_queue_t){
+    
+    }
+    
+    weak var delegate:SwiftyGCDAsyncSocketDelegate!
+    var delegateQueue:dispatch_queue_t!
+    
+    
+    func getDelegate(delegatePtr:UnsafeMutablePointer<SwiftyGCDAsyncSocketDelegate>, delegateQueue delegateQueuePtr:UnsafeMutablePointer<dispatch_queue_t>){
+        
+    }
+    func setDelegate(delegate:SwiftyGCDAsyncSocketDelegate , delegateQueue:dispatch_queue_t){
+    
+    }
+    
+    private(set) var isIPv4Enabled:Bool = true
+    private(set) var isIPv6Enabled:Bool = true
+    private(set) var isIPv4PreferredOverIPv6:Bool = true
+    var userData:AnyObject!;
+    
+    func accept(onPort port:UInt16, error errPtr:AutoreleasingUnsafeMutablePointer<NSError>) ->Bool{
+        
+        return false
+        
+    }
+
+    func accept(onInterface interface:String, port:Int16,error errPtr:AutoreleasingUnsafeMutablePointer<NSError>)->Bool{
+    return false
+    }
+    
+    func accept(onUrl url:NSURL, error errPtr:AutoreleasingUnsafeMutablePointer<NSError>)->Bool{
+    return false
+    }
+
+
+    
+
+    
+
 }
